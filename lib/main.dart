d2797f97b5e98752fd37b2eb31d9667658f67e79
Joaquin Molina <joaquin.waldrop@gmail.com>
2020-08-22 03:03:37 -0500

    Adding home view with list

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Teravision Recruitment Assistant',
+        theme: ThemeData(fontFamily: 'Poppins'),
         debugShowCheckedModeBanner: false,
         initialRoute: '/',
         routes: getApplicationRoutes());
